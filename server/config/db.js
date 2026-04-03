8c1803e5d6606738335518ca049ae0f7e3a7c30c
Siddheshwari Dilip Sonar <siddheshwaridsonar@gmail.com>
2026-04-03 23:15:54 +0530

    Create MongoDB connection configuration file

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'your_mongodb_connection_string'; // Replace with your MongoDB connection string
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+    }
+}
+
+module.exports = { client, connect };
